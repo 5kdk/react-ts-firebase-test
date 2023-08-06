6869e45dc96595459b031077102d3ce0a1e001f2
5kdk <5kdk.code@gmail.com>
2023-08-06 20:09:09 +0900

    feat: db 데이터 add 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { Auth } from './components/Auth';
 import { db } from './config/firebase';
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, addDoc } from 'firebase/firestore';
 
 interface Movie {
   id: string;
@@ -14,30 +14,65 @@ interface Movie {
 function App() {
   const [movieList, setMovieList] = useState<Movie[]>([]);
 
+  const [newMovieTitle, setNewMovieTitle] = useState('');
+  const [newReleaseDate, setNewReleaseDate] = useState(0);
+  const [isNewMovieOscar, setIsNewMovieOscar] = useState(false);
+
   const moviesCollectionRef = collection(db, 'movies');
 
-  useEffect(() => {
-    const getMovieList = async () => {
-      try {
-        const data = await getDocs(moviesCollectionRef);
-        const filteredData = data.docs.map(doc => ({
-          ...doc.data(),
-          id: doc.id,
-        })) as Movie[]; // Use the 'as Movie[]' assertion to tell TypeScript the type of filteredData
-        setMovieList(filteredData);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+  const getMovieList = async () => {
+    try {
+      const data = await getDocs(moviesCollectionRef);
+      const filteredData = data.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id,
+      })) as Movie[]; // Use the 'as Movie[]' assertion to tell TypeScript the type of filteredData
+      setMovieList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
+  useEffect(() => {
     getMovieList();
   }, []);
+
+  const onSubmitMovie = async () => {
+    try {
+      await addDoc(moviesCollectionRef, {
+        title: newMovieTitle,
+        releaseDate: newReleaseDate,
+        receivedAnOscar: isNewMovieOscar,
+      });
+
+      getMovieList();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <>
       <div>
         <Auth />
-
+        <div>
+          <input
+            placeholder="Movie title..."
+            onChange={e => setNewMovieTitle(e.target.value)}
+          />
+          <input
+            placeholder="Release Date..."
+            type="number"
+            onChange={e => setNewReleaseDate(+e.target.value)}
+          />
+          <input
+            type="checkbox"
+            checked={isNewMovieOscar}
+            onChange={e => setIsNewMovieOscar(e.target.checked)}
+          />
+          <label>Received an Oscar</label>
+          <button onClick={onSubmitMovie}>Submit Movie</button>
+        </div>
         <div>
           {movieList.map(movie => {
             return (
